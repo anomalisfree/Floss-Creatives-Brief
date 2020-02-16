8629d57a3d7e2c526f89926a6bcc5cecdf9e0335
anomalisfree <anomalisfree@gmail.com>
2020-02-17 01:58:22 +0300

    Added helicopter selector

---
+++ Assets/HelicopterSelector.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+
+public class HelicopterSelector : MonoBehaviour
+{
+    public float rotationSpeed = 500;
+    public int currentHelicopter;
+
+    private Quaternion _startRotation;
+    private Vector3 _startTouchPosition;
+    private float _targetAngle;
+
+    private void Update()
+    {
+        if (Input.touchCount == 1)
+        {
+            var touch = Input.GetTouch(0);
+
+            if (touch.phase == TouchPhase.Began)
+            {
+                _startTouchPosition = touch.position;
+                _startRotation = transform.localRotation;
+            }
+
+            if (touch.phase == TouchPhase.Moved)
+            {
+                transform.localRotation = _startRotation;
+                transform.Rotate(Vector3.up, (_startTouchPosition.x - touch.position.x) * rotationSpeed / Screen.width);
+                
+                if ((transform.localRotation.eulerAngles.y >= 0 && transform.localRotation.eulerAngles.y < 60) ||
+                    transform.localRotation.eulerAngles.y > 300)
+                {
+                    SelectHelicopter(0);
+                }
+                else if ((transform.localRotation.eulerAngles.y >= 60 && transform.localRotation.eulerAngles.y < 180))
+                {
+                    SelectHelicopter(1);
+                }
+                else if ((transform.localRotation.eulerAngles.y >= 180 && transform.localRotation.eulerAngles.y < 300))
+                {
+                    SelectHelicopter(2);
+                }
+            }
+        }
+        else
+        {
+           transform.localRotation = Quaternion.Lerp(transform.localRotation, Quaternion.Euler(0, _targetAngle, 0),
+                Time.deltaTime * 10);
+        }
+    }
+
+    private void SelectHelicopter(int number)
+    {
+        currentHelicopter = number;
+        
+        switch (number)
+        {
+            case 0:
+                _targetAngle = 0f;
+                break;
+            case 1:
+                _targetAngle = 120f;
+                break;
+            case 2:
+                _targetAngle = 240f;
+                break;
+        }
+    }
+}
---
+++ Assets/Scripts/PlaceOnPlane.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+
+public class PlaceOnPlane : MonoBehaviour
+{
+    public GameObject prefabToPlace;
+    private ARRaycastManager _sessionOrigin;
+    private List<ARRaycastHit> _hits;
+
+    private void Start()
+    {
+        _sessionOrigin = GetComponent<ARRaycastManager>();
+        _hits = new List<ARRaycastHit>();
+    }
+
+    private void Update()
+    {
+        if (Input.touchCount > 0)
+        {
+            var touch = Input.GetTouch(0);
+
+            if (touch.phase == TouchPhase.Began)
+            {
+                if (_sessionOrigin.Raycast(touch.position, _hits, TrackableType.PlaneWithinPolygon))
+                {
+                    var pose = _hits[0].pose;
+                    Instantiate(prefabToPlace, pose.position, pose.rotation);
+                }
+            }
+        }
+    }
+}
