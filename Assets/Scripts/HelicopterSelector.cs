b5d634ad065159a600e43abfba652c5ec8c5c0eb
lisovetsa <a.lisovets@hypervsn.com>
2020-02-17 12:46:12 +0300

    Select helicopter updated

--- Assets/Scripts/HelicopterSelector.cs
+++ Assets/Scripts/HelicopterSelector.cs
@@ -1,13 +1,21 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class HelicopterSelector : MonoBehaviour
 {
     public float rotationSpeed = 500;
+    public string[] helicoptersName;
+    public Text currentHelicopterName;
     public int currentHelicopter;
 
     private Quaternion _startRotation;
     private Vector3 _startTouchPosition;
     private float _targetAngle;
+
+    private void Start()
+    {
+        SelectHelicopter(0);
+    }
 
     private void Update()
     {
@@ -25,7 +33,7 @@ public class HelicopterSelector : MonoBehaviour
             {
                 transform.localRotation = _startRotation;
                 transform.Rotate(Vector3.up, (_startTouchPosition.x - touch.position.x) * rotationSpeed / Screen.width);
-                
+
                 if ((transform.localRotation.eulerAngles.y >= 0 && transform.localRotation.eulerAngles.y < 60) ||
                     transform.localRotation.eulerAngles.y > 300)
                 {
@@ -43,7 +51,7 @@ public class HelicopterSelector : MonoBehaviour
         }
         else
         {
-           transform.localRotation = Quaternion.Lerp(transform.localRotation, Quaternion.Euler(0, _targetAngle, 0),
+            transform.localRotation = Quaternion.Lerp(transform.localRotation, Quaternion.Euler(0, _targetAngle, 0),
                 Time.deltaTime * 10);
         }
     }
@@ -51,7 +59,8 @@ public class HelicopterSelector : MonoBehaviour
     private void SelectHelicopter(int number)
     {
         currentHelicopter = number;
-        
+        currentHelicopterName.text = helicoptersName[number];
+
         switch (number)
         {
             case 0:
