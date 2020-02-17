2d8729ffb4a4d4670b433f9494d5e9e4a434510f
anomalisfree <anomalisfree@gmail.com>
2020-02-18 02:40:48 +0300

    Added helicopter movement

--- Assets/Scripts/HelicopterSelector.cs
+++ Assets/Scripts/HelicopterSelector.cs
@@ -9,45 +9,69 @@ public class HelicopterSelector : MonoBehaviour
     public Text currentHelicopterName;
     public int currentHelicopter;
     public Action<int> onSelect;
+    public Transform pivot;
 
     private Quaternion _startRotation;
     private Vector3 _startTouchPosition;
+    private Vector3 _startPivotPosition;
     private float _targetAngle;
+    private float _aspectRatio;
+
+    private const float PivotPoseDelta = 5.5f;
 
     private void Start()
     {
         SelectHelicopter(0);
+        _startPivotPosition = pivot.position;
+    }
+
+    private void SetPivotPosition()
+    {
+        var newAspectRatio = (float) Screen.width / Screen.height;
+        
+        if (!(Math.Abs(_aspectRatio - newAspectRatio) > 0)) return;
+        
+        pivot.position = _startPivotPosition + Vector3.forward * PivotPoseDelta / newAspectRatio;
+        _aspectRatio = newAspectRatio;
     }
 
     private void Update()
     {
+        SetPivotPosition();
+
         if (Input.touchCount == 1)
         {
             var touch = Input.GetTouch(0);
 
-            if (touch.phase == TouchPhase.Began)
+            switch (touch.phase)
             {
-                _startTouchPosition = touch.position;
-                _startRotation = transform.localRotation;
-            }
+                case TouchPhase.Began:
+                    _startTouchPosition = touch.position;
+                    _startRotation = transform.localRotation;
+                    break;
+                case TouchPhase.Moved:
+                {
+                    transform.localRotation = _startRotation;
+                    transform.Rotate(Vector3.up,
+                        (_startTouchPosition.x - touch.position.x) * rotationSpeed / Screen.width);
 
-            if (touch.phase == TouchPhase.Moved)
-            {
-                transform.localRotation = _startRotation;
-                transform.Rotate(Vector3.up, (_startTouchPosition.x - touch.position.x) * rotationSpeed / Screen.width);
+                    if ((transform.localRotation.eulerAngles.y >= 0 && transform.localRotation.eulerAngles.y < 60) ||
+                        transform.localRotation.eulerAngles.y > 300)
+                    {
+                        SelectHelicopter(0);
+                    }
+                    else if ((transform.localRotation.eulerAngles.y >= 60 &&
+                              transform.localRotation.eulerAngles.y < 180))
+                    {
+                        SelectHelicopter(1);
+                    }
+                    else if ((transform.localRotation.eulerAngles.y >= 180 &&
+                              transform.localRotation.eulerAngles.y < 300))
+                    {
+                        SelectHelicopter(2);
+                    }
 
-                if ((transform.localRotation.eulerAngles.y >= 0 && transform.localRotation.eulerAngles.y < 60) ||
-                    transform.localRotation.eulerAngles.y > 300)
-                {
-                    SelectHelicopter(0);
-                }
-                else if ((transform.localRotation.eulerAngles.y >= 60 && transform.localRotation.eulerAngles.y < 180))
-                {
-                    SelectHelicopter(1);
-                }
-                else if ((transform.localRotation.eulerAngles.y >= 180 && transform.localRotation.eulerAngles.y < 300))
-                {
-                    SelectHelicopter(2);
+                    break;
                 }
             }
         }
---
+++ Assets/Scripts/StartPositionManager.cs
@@ -0,0 +1,147 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+
+public class StartPositionManager : MonoBehaviour
+{
+    public GameObject[] helicopterPrefabs;
+    public HelicopterSelector helicopterSelector;
+    public float rotationSpeed = 500;
+    public GameObject canvasPose;
+
+    private GameObject _currentHelicopter;
+    private ARRaycastManager _sessionOrigin;
+    private List<ARRaycastHit> _hits;
+
+    private int _currentMode;
+    private int _currentHelicopterNum;
+
+    private bool _isInRotation;
+    private Vector3 _startTouchPosition;
+    private Quaternion _startRotation;
+
+    private float _startTouchDestination;
+    private Vector3 _startScale;
+
+    private void Awake()
+    {
+        _sessionOrigin = GetComponent<ARRaycastManager>();
+        _hits = new List<ARRaycastHit>();
+
+        helicopterSelector.onSelect += SetPositionMode;
+    }
+
+    private void Update()
+    {
+        switch (_currentMode)
+        {
+            case 0:
+
+                break;
+            case 1:
+                InPositionMode();
+                break;
+            case 2:
+
+                break;
+        }
+    }
+
+    private void InPositionMode()
+    {
+        if (Input.touchCount == 1)
+        {
+            var touch = Input.GetTouch(0);
+            var pose = GetSessionOriginRayPose(touch);
+
+            if (touch.phase == TouchPhase.Began)
+            {
+                if (_isInRotation)
+                {
+                    _startTouchPosition = touch.position;
+                    _startRotation = _currentHelicopter.transform.localRotation;
+                }
+                else
+                {
+                    SetHelicopterPose(pose);
+                }
+            }
+            else if (touch.phase == TouchPhase.Moved)
+            {
+                if (_isInRotation)
+                {
+                    _currentHelicopter.transform.localRotation = _startRotation;
+                    _currentHelicopter.transform.Rotate(Vector3.up,
+                        (_startTouchPosition.x - touch.position.x) * rotationSpeed / Screen.width);
+                }
+                else
+                {
+                    SetHelicopterPose(pose);
+                }
+            }
+        }
+        else if (_isInRotation && Input.touchCount == 2 && _currentHelicopter != null)
+        {
+            var touchOne = Input.GetTouch(0);
+            var touchTwo = Input.GetTouch(1);
+
+            if (touchTwo.phase == TouchPhase.Began)
+            {
+                _startTouchDestination = Vector2.Distance(touchOne.position, touchTwo.position);
+                _startScale = _currentHelicopter.transform.localScale;
+            }
+            else if (touchOne.phase == TouchPhase.Moved || touchTwo.phase == TouchPhase.Moved)
+            {
+                _currentHelicopter.transform.localScale = _startScale + Vector3.one *
+                    (Vector2.Distance(touchOne.position, touchTwo.position) -
+                     _startTouchDestination)  / Screen.width;
+            }
+        }
+    }
+
+    private Pose GetSessionOriginRayPose(Touch touch)
+    {
+        return _sessionOrigin.Raycast(touch.position, _hits, TrackableType.PlaneWithinPolygon)
+            ? _hits[0].pose
+            : Pose.identity;
+    }
+
+    private void SetHelicopterPose(Pose pose)
+    {
+        if (pose == Pose.identity) return;
+
+        if (_currentHelicopter == null)
+        {
+            _currentHelicopter =
+                Instantiate(helicopterPrefabs[_currentHelicopterNum], pose.position, pose.rotation);
+            _currentHelicopter.transform.position = pose.position;
+            _currentHelicopter.transform.rotation = pose.rotation;
+
+            canvasPose.SetActive(true);
+            _isInRotation = true;
+        }
+        else
+        {
+            _currentHelicopter.transform.position = pose.position;
+            _currentHelicopter.transform.rotation = pose.rotation;
+        }
+    }
+
+    private void SetPositionMode(int currentHelicopterNum)
+    {
+        _currentHelicopterNum = currentHelicopterNum;
+        _currentMode = 1;
+        helicopterSelector.transform.parent.parent.gameObject.SetActive(false);
+
+        // _currentHelicopter =
+        //     Instantiate(helicopterPrefabs[_currentHelicopterNum], new Vector3(0, 0, 5), Quaternion.identity);
+        // _isInRotation = true;
+    }
+
+    public void SetMovement()
+    {
+        _isInRotation = false;
+        canvasPose.SetActive(false);
+    }
+}
