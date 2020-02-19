56fa884670f81311faaa66c96d2056ca90dcf9fd
lisovetsa <a.lisovets@hypervsn.com>
2020-02-19 17:07:32 +0300

    Added bubbles

---
+++ Assets/BubbleController.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+public class BubbleController : MonoBehaviour
+{
+    private Animator _animator;
+    private static readonly int Show = Animator.StringToHash("show");
+
+    private void Awake()
+    {
+        _animator = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        if (Camera.main != null) transform.LookAt(Camera.main.transform);
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.transform.CompareTag($"CameraCollider"))
+        {
+            _animator.SetBool(Show, true);
+        }
+        
+        Debug.Log("OnCollisionEnter " + other.transform.tag);
+    }
+
+    private void OnCollisionExit(Collision other)
+    {
+        if (other.transform.CompareTag($"CameraCollider"))
+        {
+            _animator.SetBool(Show, false);
+        }
+    }
+}
